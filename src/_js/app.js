8cfa9f61baed11f17e730dcc7e575a8d155b03a7
Kong Yim <kong.yim@imagination.com>
2018-05-23 17:40:16 +0800

    ADD: refactor js code

---
+++ src/_js/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+import angular from 'angular'
+import 'angular-route'
+import 'angular-animate'
+import 'angular-sanitize'
+
+import './app.config'
+import './app.core'
+import './app.routes'
+import './app.services'
+
+import * as Swiper from '../assets/js/swiper.min.js'
+window.Swiper = Swiper
+
+import '../components/navbar/navbar.ctrl'
+import '../components/swiper/swiper.ctrl'
+import '../components/fixedbanner/fixedbanner.ctrl'
+import '../sections/home/home.ctrl'
+import '../sections/aboutus/aboutus.ctrl'
+import '../sections/solutions/solutions.ctrl'
+
+angular.module('app', ['ngRoute', 'ngAnimate', 'ngSanitize', 'app.routes', 'app.core', 'app.services', 'app.config']);
+
--- src/app.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-angular.module('app', ['ngRoute', 'ngAnimate', 'ngSanitize', 'app.routes', 'app.core', 'app.services', 'app.config']);
-
